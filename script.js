84561010300e2f12b8d1fe05e7c8077a56ccbf5a
Hennie Scheepers <hennie.104@hotmail.com>
2023-05-22 10:12:52 +0200

    Fixed some styling issues and added 'clear' functionality

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/**
+ * Even handler for when the user clicks the 'calculate' button. It takes all the
+ * parameters needed and does a calculation according to the user's rates to generate
+ * price in a consistent manner
+ */
 const handleCalculate = (event) => {
     //Storing elements in variables
     const sizeElement = document.querySelector('[data-size-select]')
@@ -14,6 +19,7 @@ const handleCalculate = (event) => {
     //Extracted size value
     let sizeValue
     const fullFramePrice = 200 * 2
+    //Checks user selection for rug size
     switch (size) {
         case '1/4 frame' : sizeValue = 0.25
                             break
@@ -28,14 +34,40 @@ const handleCalculate = (event) => {
     const smallWoolPrice = 24 * smallWoolValue
     const largeWoolPrice = 70 * largeWoolValue
 
+    //Calculation to determine quote price
     let totalPrice = sizePrice + smallWoolPrice + largeWoolPrice + daysPrice
     const priceWithVat = Math.round(totalPrice + (totalPrice * 0.15))
 
-
+    // Adding quote price to the output
     const output = document.querySelector('[data-output]')
     output.innerHTML = `<h1 class="heading">${priceWithVat}</h1>`
 
 }
 
+/**
+ * Event handler for when the user clicks the 'clear' button. It takes all the 
+ * input fields and resets them to their initial values
+ */
+const handleClear = () => {
+    //Grab elements and stores each of them inside of a variable
+    const sizeElement = document.querySelector('[data-size-select]')
+    const woolLargeElement = document.querySelector('[data-wool-big-bundles]')
+    const woolSmallElement = document.querySelector('[data-wool-small-bundles]')
+    const daysElement = document.querySelector('[data-days]')
+
+    //Resets the element's values
+    sizeElement.value = '1/4 frame'
+    woolLargeElement.value = ''
+    woolSmallElement.value = ''
+    daysElement.value = ''
+
+    //Clears the output
+    const output = document.querySelector('[data-output]')
+    output.innerHTML = ``
+}
+
 const calculateButton = document.querySelector('[data-calculate-button]')
 calculateButton.addEventListener('click', handleCalculate)
+
+const clearButton = document.querySelector('[data-clear-button]')
+clearButton.addEventListener('click', handleClear)
